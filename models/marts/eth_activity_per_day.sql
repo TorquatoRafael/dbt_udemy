958c183eba24b9aa03b5e1ea4b7f795c844f5ff6
Rafael Torquato <rafastorquato@hotmail.com>
2026-04-20 16:36:18 -0300

    Macros e DRY

--- models/marts/eth_activity_per_day.sql
+++ models/marts/eth_activity_per_day.sql
@@ -9,7 +9,7 @@ with transactions as (
         date
         , transaction_category
         , count(*) as tx_count
-        , sum(value/1e18) as sum_eth_value
+        , {{ conversion('value', '18') }} as sum_eth_value
     from {{ ref('stg_transactions_enriched')}}
     group by date, transaction_category
 )
